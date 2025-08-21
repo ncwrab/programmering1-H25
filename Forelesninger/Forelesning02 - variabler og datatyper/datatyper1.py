d38f229448022997e8a842fa0e5eb6e88274cea1
ncwrab <143709905+ncwrab@users.noreply.github.com>
2025-08-21 10:33:38 +0200

    Create datatyper1.py

---
+++ Forelesninger/Forelesning02 - variabler og datatyper/datatyper1.py
@@ -0,0 +1,9 @@
+variabel = 5            # Innholdet i variabelen er av datatypen int (heltall)
+print(variabel)
+
+
+# Hvilke verdier vil bli printet ut? 
+number = 2
+print(number)
+number = 6
+print(number)
