93075559bc59956384a3d79de141be28d0c146c2
ncwrab <143709905+ncwrab@users.noreply.github.com>
2025-08-28 10:53:37 +0200

    Create if-test01.py

---
+++ Forelesninger/Forelesning04 - operatorer, if-setninger/if-test01.py
@@ -0,0 +1,9 @@
+number = 0      # Endre dette tallet for å teste forskjellige verdier og se hva som skjer når if-testen 'kjøres'. Prøv f.eks med noen positive og noen negative tall
+
+# OBS. Merk at indenteringen/innrykk er helt nødvendig å ha med i if-tester.
+# Innrykket viser at en kodeblokk 'tilhører' if-testen. 
+# Legg også merke til at vi må ha med : etter betingelsen.
+
+# Sjekker om tallet er positivt (større enn 0)
+if (number > 0):
+    print(f"{number} er et positivt tall")
